6dcb253363036683b691d3766ff0366f38ccba11
Bianca Rosa <bmendonca@stone.com.br>
2017-06-26 15:16:36 -0300

    add host

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def test_disconnect(sid):
     print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(port=int(os.getenv('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
