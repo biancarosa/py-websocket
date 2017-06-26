933629604ddf0065d5e315b479e08b97c45f2098
Bianca Rosa <me@biancarosa.com.br>
2017-06-25 23:07:07 -0300

    Remove room-based events

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import asyncio
+import socketio
+import logging
 
 from sanic import Sanic
-from sanic.response import html
-
-import socketio
 
 sio = socketio.AsyncServer(async_mode='sanic')
 app = Sanic()
@@ -24,45 +23,10 @@ async def background_task():
 def before_server_start(sanic, loop):
     sio.start_background_task(background_task)
 
-
 @sio.on('my event', namespace='/test')
 async def test_message(sid, message):
     await sio.emit('my response', {'data': message['data']}, room=sid,
                    namespace='/test')
-
-
-@sio.on('my broadcast event', namespace='/test')
-async def test_broadcast_message(sid, message):
-    await sio.emit('my response', {'data': message['data']}, namespace='/test')
-
-
-@sio.on('join', namespace='/test')
-async def join(sid, message):
-    sio.enter_room(sid, message['room'], namespace='/test')
-    await sio.emit('my response', {'data': 'Entered room: ' + message['room']},
-                   room=sid, namespace='/test')
-
-
-@sio.on('leave', namespace='/test')
-async def leave(sid, message):
-    sio.leave_room(sid, message['room'], namespace='/test')
-    await sio.emit('my response', {'data': 'Left room: ' + message['room']},
-                   room=sid, namespace='/test')
-
-
-@sio.on('close room', namespace='/test')
-async def close(sid, message):
-    await sio.emit('my response',
-                   {'data': 'Room ' + message['room'] + ' is closing.'},
-                   room=message['room'], namespace='/test')
-    await sio.close_room(message['room'], namespace='/test')
-
-
-@sio.on('my room event', namespace='/test')
-async def send_room_message(sid, message):
-    await sio.emit('my response', {'data': message['data']},
-                   room=message['room'], namespace='/test')
-
 
 @sio.on('disconnect request', namespace='/test')
 async def disconnect_request(sid):
@@ -71,6 +35,7 @@ async def disconnect_request(sid):
 
 @sio.on('connect', namespace='/test')
 async def test_connect(sid, environ):
+    logging.INFO("User {} logged in".format(sid))
     await sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
                    namespace='/test')
 
